4ef83e022ea5928068ad2b81258068878dae65b9
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-07-05 16:03:46 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,13 @@ var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
 
-const OWM_API_KEY = process.env.OWM_API_KEY;
-
 router.get("/movies", (req, res) => {
   const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${OWM_API_KEY}`,
+      Authorization: `Bearer ${process.env.OWM_API_KEY}`,
     },
   };
   fetch(url, options)
